3284ee7aaf53584f98661c6b841f99f4e4a2dc30
gunhee <120537859+gunhee0421@users.noreply.github.com>
2024-07-21 12:01:35 +0900

    Title: 배열의 특정 위치 찾기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240721/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 240721/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -3,7 +3,13 @@ arr = list(map(int, input().split()))
 n = len(arr)
 
 sumVal = 0
+threeVal = []
 for i in range(1, n, 2):
     sumVal += arr[i]
+for i in range(2, n, 3):
+    threeVal.append(arr[i])
 
-print(sumVal, sumVal/(len(arr)/2))
+three = sum(threeVal)/len(threeVal)
+
+
+print(sumVal, round(three, 1))
