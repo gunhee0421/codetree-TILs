5a15bd8de385b4866e2ceb3d105a3f33e0f58b61
gunhee <120537859+gunhee0421@users.noreply.github.com>
2024-07-22 13:55:17 +0900

    Title: 돌 잘 치우기, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240722/돌 잘 치우기/clear-stones-well.py
+++ 240722/돌 잘 치우기/clear-stones-well.py
@@ -2,27 +2,21 @@ from itertools import combinations
 
 def solution(n, k, m, arr, p):
     def bfs(position, graph):
-        # visited = [[False]*n for _ in range(n)]
-        # stack = [(position[0]-1, position[1]-1)]
-        # visited[position[0]-1][position[1]-1] = True
-        # count=0
-
-        # while stack:
-        #     cx, cy = stack.pop()
-        #     count+=1
-        #     direction = [(-1,0),(1,0),(0,1),(0,-1)]
-        #     for dx, dy in direction:
-        #         nx, ny = cx+dx, cy+dy
-        #         if 0<=nx<n and 0<=ny<n and not visited[nx][ny] and graph[nx][ny]==0:
-        #             visited[nx][ny] = True
-        #             stack.append((nx, ny))
-        # return count
+        visited = [[False]*n for _ in range(n)]
+        stack = [(position[0]-1, position[1]-1)]
+        visited[position[0]-1][position[1]-1] = True
         count=0
-        for i in range(n):
-            for j in range(n):
-                if graph[i][j] == 0:
-                    count+=1
-        return count;
+
+        while stack:
+            cx, cy = stack.pop()
+            count+=1
+            direction = [(-1,0),(1,0),(0,1),(0,-1)]
+            for dx, dy in direction:
+                nx, ny = cx+dx, cy+dy
+                if 0<=nx<n and 0<=ny<n and not visited[nx][ny] and graph[nx][ny]==0:
+                    visited[nx][ny] = True
+                    stack.append((nx, ny))
+        return count
 
     stone_position = [(i, j) for i in range(n) for j in range(n) if graph[i][j] == 1]
     max_reach = 0
