d603612f83c910d7e0ac5a2a1f749571a3663eb9
gunhee <120537859+gunhee0421@users.noreply.github.com>
2024-07-23 14:33:11 +0900

    Title: 빙하, Time: 178ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240723/빙하/glacier.py
+++ 240723/빙하/glacier.py
@@ -6,35 +6,36 @@ def melt_glacier(n, m, grid):
     def is_within_bounds(x, y):
         return 0 <= x < n and 0 <= y < m
 
-    def bfs(start):
-        queue = deque([start])
-        visited = set([start])
-        while queue:
-            cx, cy = queue.popleft()
-            for dx, dy in directions:
-                nx, ny = cx + dx, cy + dy
-                if is_within_bounds(nx, ny) and (nx, ny) not in visited and grid[nx][ny] == 0:
-                    visited.add((nx, ny))
-                    queue.append((nx, ny))
-        return visited
+    def add_to_queue_if_boundary(x, y, queue, visited):
+        if is_within_bounds(x, y) and (x, y) not in visited and grid[x][y] == 0:
+            visited.add((x, y))
+            queue.append((x, y))
 
     time = 0
     last_melt_size = 0
     
-    water_boundary = set()
+    water_boundary = deque()
+    visited = set()
     for i in range(n):
         for j in range(m):
             if grid[i][j] == 0 and (i == 0 or i == n-1 or j == 0 or j == m-1):
-                water_boundary.update(bfs((i, j)))
+                water_boundary.append((i, j))
+                visited.add((i, j))
 
     while True:
         to_melt = set()
         new_water_boundary = set()
-        for x, y in water_boundary:
+
+        for _ in range(len(water_boundary)):
+            x, y = water_boundary.popleft()
             for dx, dy in directions:
                 nx, ny = x + dx, y + dy
-                if is_within_bounds(nx, ny) and grid[nx][ny] == 1:
-                    to_melt.add((nx, ny))
+                if is_within_bounds(nx, ny):
+                    if grid[nx][ny] == 1:
+                        to_melt.add((nx, ny))
+                    elif (nx, ny) not in visited:
+                        visited.add((nx, ny))
+                        water_boundary.append((nx, ny))
 
         if not to_melt:
             break
@@ -42,9 +43,8 @@ def melt_glacier(n, m, grid):
         last_melt_size = len(to_melt)
         for x, y in to_melt:
             grid[x][y] = 0
-            new_water_boundary.update(bfs((x, y)))
+            add_to_queue_if_boundary(x, y, water_boundary, visited)
 
-        water_boundary = new_water_boundary
         time += 1
 
     return time, last_melt_size
